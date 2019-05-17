7c9343250b1df9a95fd800635a3ede076f805d89
Nandini Bulusu <nanbul785@gmail.com>
2019-05-17 10:48:31 -0700

    Setting up authentication

---
+++ src/components/Firebase/fire.js
@@ -0,0 +1,33 @@
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import firebase from 'firebase';
+import app from 'firebase/app';
+
+// Add the Firebase services that you want to use
+import 'firebase/auth';
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+// Initialize Firebase
+class Firebase {
+  constructor() {
+    app.initializeApp(firebaseConfig);
+
+    this.auth = app.auth();
+  }
+
+  // Auth API
+  doCreateUserWithEmailAndPassword = (email, password) =>
+  this.auth.createUserWithEmailAndPassword(email,password);
+}
+
+export default Firebase;
